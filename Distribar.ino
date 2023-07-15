eef09a0171d02e01008f26be66267c242491705f
Etienne Païta <etienne@studio-equinoxe.com>
2023-07-15 15:56:18 +0200

    Update : Distribar cleaning code

--- Distribar.ino
+++ Distribar.ino
@@ -1,14 +1,3 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
-
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #if defined(ESP32)
@@ -18,23 +7,16 @@
 #endif
 #include <Firebase_ESP_Client.h>
 
-
-//Provide the token generation process info.
 #include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
 // Machine ID
 #define UID "295f8f74-18d9-11ee-be56-0242ac120002"
 
-// Insert your network credentials
-#define WIFI_SSID "XXXX-5ab5"
-#define WIFI_PASSWORD "fnx4dpdsvtwh"
+#define WIFI_SSID "iPhone de Elsa"//"XXXX-5ab5"
+#define WIFI_PASSWORD "azertyui"//"fnx4dpdsvtwh"
 
-// Insert Firebase project API Key
 #define API_KEY "AIzaSyCcQYX68LOS7dW939v9RTejZI1I21rr5WU"
-
-// Insert RTDB URLefine the RTDB URL */
 #define DATABASE_URL "https://distrib-ar-default-rtdb.europe-west1.firebasedatabase.app/" 
 
 #define GPIO_D0 16
@@ -47,12 +29,15 @@
 #define GPIO_D7 13
 #define GPIO_D8 15
 
-#define COCKTAIL_DOSE_DELAY 1000 //in millis
+#define CLEAN_DELAY 60000 //in millis
+#define COCKTAIL_DOSE_DELAY 20000 //in millis
+#define NEW_CONFIG_DELAY 5000 //in millis
 #define DEST_SIZE 50
 
 //Define Firebase Data object
 FirebaseData configFBDO;
 FirebaseData cocktailsFBDO;
+FirebaseData cleaningFBDO;
 FirebaseData rndFBDO;
 
 FirebaseAuth auth;
@@ -60,6 +45,7 @@ FirebaseConfig config;
 
 char fbdoConfigPath[DEST_SIZE] = "/";
 char fbdoCocktailsPath[DEST_SIZE] = "/";
+char fbdoCleaningPath[DEST_SIZE] = "/";
 
 // Distance sensor
 /* Constantes pour le timeout */
@@ -81,36 +67,32 @@ bool cocktailOnCreation = false;
 FirebaseJsonArray cocktail;
 bool cocktailAvailable = false;
 bool cocktailWaiting = false;
+bool cleanFinished = false;
 
 void startAssociatedPump(String data) {
   Serial.print("Turning on ... ");
 
   if(barConfig["gpio1"] == data) {
-    //Serial.println(barConfig["gpio1"]);
     digitalWrite(GPIO_D5, HIGH);
     delay(COCKTAIL_DOSE_DELAY);
     digitalWrite(GPIO_D5, LOW);
   }
   else if(barConfig["gpio2"] == data) {
-    //Serial.println(barConfig["gpio2"]);
     digitalWrite(GPIO_D6, HIGH);
     delay(COCKTAIL_DOSE_DELAY);
     digitalWrite(GPIO_D6, LOW);
   }
   else if(barConfig["gpio3"] == data) {
-    //Serial.println(barConfig["gpio3"]);
     digitalWrite(GPIO_D3, HIGH);
     delay(COCKTAIL_DOSE_DELAY);
     digitalWrite(GPIO_D3, LOW);
   }
   else if(barConfig["gpio4"] == data) {
-    //Serial.println(barConfig["gpio4"]);
     digitalWrite(GPIO_D2, HIGH);
     delay(COCKTAIL_DOSE_DELAY);
     digitalWrite(GPIO_D2, LOW);
   }
   else if(barConfig["gpio5"] == data) {
-    //Serial.println(barConfig["gpio5"]);
     digitalWrite(GPIO_D1, HIGH);
     delay(COCKTAIL_DOSE_DELAY);
     digitalWrite(GPIO_D1, LOW);
@@ -147,43 +129,33 @@ void createAwesomeCocktail() {
   cocktailWaiting = true;
 }
 
-void showConfiguration() {
-  const char* gpio1 = barConfig["gpio1"];
-  const char* gpio2 = barConfig["gpio2"];
-  const char* gpio3 = barConfig["gpio3"];
-  const char* gpio4 = barConfig["gpio4"];
-  const char* gpio5 = barConfig["gpio5"];
+void updateCleanAttribute() {
+  if (Firebase.RTDB.setInt(&cleaningFBDO,fbdoCleaningPath,0)){
+    Serial.println("Clean attributes changed");
+  } else {
+    Serial.println("Failure : clean attributes can not be changed");
+  }
+  cleanFinished = false;
+}
 
-  if(strlen(gpio1) > 0) {
-    Serial.println(gpio1);
-    digitalWrite(GPIO_D5, HIGH);
-    delay(1000);
-    digitalWrite(GPIO_D5, LOW);
-  }
-  if(strlen(gpio2) > 0) {
-    Serial.println(gpio2);
-    digitalWrite(GPIO_D6, HIGH);
-    delay(1000);
-    digitalWrite(GPIO_D6, LOW);
-  }
-  if(strlen(gpio3) > 0) {
-    Serial.println(gpio3);
-    digitalWrite(GPIO_D3, HIGH);
-    delay(1000);
-    digitalWrite(GPIO_D3, LOW);
-  }
-  if(strlen(gpio4) > 0) {
-    Serial.println(gpio4);
-    digitalWrite(GPIO_D2, HIGH);
-    delay(1000);
-    digitalWrite(GPIO_D2, LOW);
-  }
-  if(strlen(gpio5) > 0) {
-    Serial.println(gpio5);
-    digitalWrite(GPIO_D1, HIGH);
-    delay(1000);
-    digitalWrite(GPIO_D1, LOW);
-  }
+void cleanMachine() {
+  Serial.println("Cleaning...");
+
+  digitalWrite(GPIO_D5, HIGH);
+  digitalWrite(GPIO_D6, HIGH);  
+  digitalWrite(GPIO_D3, HIGH);
+  digitalWrite(GPIO_D2, HIGH);
+  digitalWrite(GPIO_D1, HIGH);
+
+  delay(CLEAN_DELAY);
+  
+  digitalWrite(GPIO_D5, LOW);
+  digitalWrite(GPIO_D6, LOW);
+  digitalWrite(GPIO_D3, LOW);
+  digitalWrite(GPIO_D2, LOW);
+  digitalWrite(GPIO_D1, LOW);
+
+  cleanFinished = true;
 }
 
 void saveConfiguration(String jsonConfig) {
@@ -201,92 +173,73 @@ void saveConfiguration(String jsonConfig) {
 void updateConfig(String dataPath, String newData) {
   if (dataPath == "/gpio1") {
     barConfig["gpio1"] = newData;
+    digitalWrite(GPIO_D5, HIGH);
+    delay(NEW_CONFIG_DELAY);
+    digitalWrite(GPIO_D5, LOW);
   } else if (dataPath == "/gpio2") {
     barConfig["gpio2"] = newData;
+    digitalWrite(GPIO_D6, HIGH);
+    delay(NEW_CONFIG_DELAY);
+    digitalWrite(GPIO_D6, LOW);
   } else if (dataPath == "/gpio3") {
     barConfig["gpio3"] = newData;
+    digitalWrite(GPIO_D3, HIGH);
+    delay(NEW_CONFIG_DELAY);
+    digitalWrite(GPIO_D3, LOW);
   } else if (dataPath == "/gpio4") {
     barConfig["gpio4"] = newData;
+    digitalWrite(GPIO_D2, HIGH);
+    delay(NEW_CONFIG_DELAY);
+    digitalWrite(GPIO_D2, LOW);
   } else if (dataPath == "/gpio5") {
     barConfig["gpio5"] = newData;
+    digitalWrite(GPIO_D1, HIGH);
+    delay(NEW_CONFIG_DELAY);
+    digitalWrite(GPIO_D1, LOW);
   }
 }
 
 
 void configStreamCallback(FirebaseStream data) {
-  /*
-  showLed = true;
-  ledOnPreviousMillis = millis();
-  digitalWrite(D1, HIGH);
-  */
-
-  // Print out all information
-
   Serial.println(" --- Config stream Data...");
   Serial.println(data.streamPath());
-  Serial.println(data.dataPath());
-  Serial.println(data.dataType());
-
-  //Serial.print("Json data = ");
-  //Serial.println(data.stringValue());
-
-  // Print out the value
-  // Stream data can be many types which can be determined from function dataType
-
 
   if (data.dataTypeEnum() == fb_esp_rtdb_data_type_string) {
-    //Configuration part update
     Serial.println(data.to<String>());
     updateConfig(data.dataPath(),data.to<String>());
-    showConfiguration();
   }
   else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json)
   {
-    // On get all configuration
     FirebaseJson *json = data.to<FirebaseJson *>();
     Serial.println(json->raw());
     saveConfiguration(data.stringData());
-    showConfiguration();
   }
 }
 
 void cocktailsStreamCallback(FirebaseStream data) {
-
-  // Print out all information
-
   Serial.println(" --- Cocktails stream Data...");
   Serial.print(data.streamPath());
+
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_array)
+  {
+    cocktail = data.to<FirebaseJsonArray>();
+    cocktailAvailable = true;
+    Serial.println(cocktail.raw());
+  }
+}
+
+void cleaningStreamCallback(FirebaseStream data) {
+  Serial.println(" --- Cleaning stream Data...");
+  Serial.println(data.streamPath());
   Serial.println(data.dataPath());
   Serial.println(data.dataType());
 
-  // Print out the value
-  // Stream data can be many types which can be determined from function dataType
-
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer)
-      Serial.println(data.to<int>());
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_float)
-      Serial.println(data.to<float>(), 5);
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_double)
-      printf("%.9lf\n", data.to<double>());
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_boolean)
-      Serial.println(data.to<bool>()? "true" : "false");
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_string)
-      Serial.println(data.to<String>());
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json)
-  {
-      FirebaseJson *json = data.to<FirebaseJson *>();
-      Serial.println(json->raw());
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer) {
+    Serial.println(data.to<int>());
+    if (data.to<int>() == 1) {
+      cleanMachine();
+    }
   }
-  else if (data.dataTypeEnum() == fb_esp_rtdb_data_type_array)
-  {
-    //Should always get an array
-      cocktail = data.to<FirebaseJsonArray>();
-      cocktailAvailable = true;
-      Serial.println(cocktail.raw());
-      
-  }
-     
-
 }
 
 // Global function that notifies when stream connection lost
@@ -346,6 +299,7 @@ void setup(){
 
   Firebase.RTDB.setStreamCallback(&configFBDO, configStreamCallback, streamTimeoutCallback);
   Firebase.RTDB.setStreamCallback(&cocktailsFBDO, cocktailsStreamCallback, streamTimeoutCallback);
+  Firebase.RTDB.setStreamCallback(&cleaningFBDO, cleaningStreamCallback, streamTimeoutCallback);
 
   // Set RTDB config paths
   strcat(fbdoConfigPath, UID);
@@ -355,6 +309,10 @@ void setup(){
   strcat(fbdoCocktailsPath, UID);
   strcat(fbdoCocktailsPath, "/cocktails");
 
+  // Set RTDB cleaning paths
+  strcat(fbdoCleaningPath, UID);
+  strcat(fbdoCleaningPath, "/nettoyage");
+
   /* Start streams */
   if (!Firebase.RTDB.beginStream(&configFBDO, fbdoConfigPath)) {
     Serial.println(configFBDO.errorReason());
@@ -363,10 +321,18 @@ void setup(){
   if (!Firebase.RTDB.beginStream(&cocktailsFBDO, fbdoCocktailsPath)) {
     Serial.println(cocktailsFBDO.errorReason());
   }
+
+  if (!Firebase.RTDB.beginStream(&cleaningFBDO, fbdoCleaningPath)) {
+    Serial.println(cleaningFBDO.errorReason());
+  }
 }
 
 void loop(){
   if (Firebase.ready() && signupOK) {
+
+    if (cleanFinished) {
+      updateCleanAttribute();
+    }
 
     //Create cocktail if
     if (cocktailAvailable && (distance_cm < 4.0)) {
